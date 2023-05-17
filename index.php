c260ad61941930d69625a185bbba6075df61eb26
Petr Sychra <sychrapetan@seznam.cz>
2023-05-17 21:27:10 +0200

    výpis kategorií

--- index.php
+++ index.php
@@ -7,12 +7,18 @@
     $title = 'Fórum';
 
 
-    $categories = $db->query('SELECT DISTINCT categories.name AS category_name, categories.description AS description, comments.updated AS updated, COUNT(comments.comments_id) AS comments FROM categories JOIN topics USING (categories_id) JOIN comments USING (topics_id) GROUP BY categories.name ORDER BY comments.updated DESC;')->fetchAll(PDO::FETCH_ASSOC);
 
+
+
+    $categories = $db->query('SELECT DISTINCT categories.name AS category_name, categories.description AS description, categories.categories_id AS categories_id, comments.updated AS updated, COUNT(comments.comments_id) AS comments
+                                    FROM categories JOIN topics USING (categories_id) JOIN comments USING (topics_id) GROUP BY categories.name  
+                                    ORDER BY `comments`.`updated` ASC;')->fetchAll(PDO::FETCH_ASSOC);
     if (!empty($categories)){
         foreach ($categories as $category) {
-            echo '<div class="container p-5 my-5 border">
-                    <h2>'.htmlspecialchars($category['category_name']).'</h2>
+            echo '<div class="container p-3 my-3 border border-3">
+                    <h2>
+                      <a href="topics.php?category='.$category['categories_id'].'">'.htmlspecialchars($category['category_name']).'</a>
+                    </h2>
                     <p>'.htmlspecialchars($category['description']).'</p>
                     <p>Počet komentářů: '.htmlspecialchars($category['comments']).'</p>
                     <p class="text-muted">Poslední příspěvek: '.htmlspecialchars(date('d.m.Y H:i',strtotime($category['updated']))).'</p>
@@ -20,11 +26,9 @@
         }
     }
     else{
-        echo '<div class="container p-5 my-5 border">
+        echo '<div class="container p-5 my-5 border border-3">
                 <h2>Nebyly nalezeny žádné kategorie.</h2>
               </div>';
     }
-
-
 
     include __DIR__.'/include/footer.php';
