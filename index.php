3342ce377c3291c4aa7f05804645db33fab432a4
Petr Sychra <sychrapetan@seznam.cz>
2023-05-17 18:48:07 +0200

    oprava chyby

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
             echo '<div class="container p-5 my-5 border">
                     <h2>'.htmlspecialchars($category['category_name']).'</h2>
                     <p>'.htmlspecialchars($category['description']).'</p>
-                    <p>Počet komentářů: '.htmlspecialchars(date('d,m,Y H:i',strtotime($category['comments']))).'</p>
-                    <p class="text-muted">Poslední příspěvek: '.htmlspecialchars($category['updated']).'</p>
+                    <p>Počet komentářů: '.htmlspecialchars($category['comments']).'</p>
+                    <p class="text-muted">Poslední příspěvek: '.htmlspecialchars(date('d.m.Y H:i',strtotime($category['updated']))).'</p>
                   </div>';
         }
     }
