dbcc1938cf9709fe7e7fcc9e48699f8b1a2b3f57
Petr Sychra <sychrapetan@seznam.cz>
2023-05-20 12:29:01 +0200

    klikatelné kategorie a témata comentářů

--- userComments.php
+++ userComments.php
@@ -4,37 +4,43 @@
 
     include 'include/header.php';
 
-    $comments = $db->prepare('SELECT comments.comments_id, comments.text AS text, topics.name AS topic_name, categories.name AS category_name, users.username AS username, comments.created AS created, comments.updated AS updated
+    $query = $db->prepare('SELECT categories.categories_id AS categories_id, topics.topics_id AS topics_id, users.users_id AS users_id, comments.comments_id AS comments_id, comments.text AS text, topics.name AS topic_name, categories.name AS category_name, users.username AS username, comments.created AS created, comments.updated AS updated
                                     FROM users JOIN comments ON users.users_id=comments.creator_id JOIN topics ON topics.topics_id=comments.topics_id JOIN categories ON categories.categories_id=topics.categories_id
                                     WHERE users_id=:users_id
                                     ORDER BY updated DESC;');
-    $comments->execute([
+    $query->execute([
         ':users_id'=>$_SESSION['users_id']
     ]);
 
+    $comments = $query->fetchAll(PDO::FETCH_ASSOC);
+
     if (!empty($comments)){
         foreach ($comments as $comment) {
+
             echo '<div class="container p-3 my-3 border border-3">
                             <div class="row">
                                 <div class="col-8">
                                     <h2>'.htmlspecialchars($comment['username']).'</h2>
                                     <p>'.htmlspecialchars($comment['text']).'</p>
-                                    <span class="badge bg-primary">'.$comment['category_name'].'</span>
-                                    <span class="badge bg-secondary">'.$comment['topic_name'].'</span>
+                                    <a href="showTopics.php?id='.$comment['categories_id'].'" class="badge bg-primary">'.$comment['category_name'].'</a>
+                                    <a href="showComments.php?id='.$comment['topics_id'].'" class="badge bg-secondary">'.$comment['topic_name'].'</a>
                                 </div>
                                 <div class="col-4">
                                     <p class="text-muted">Aktualizováno: '.htmlspecialchars(date('d.m.Y H:i',strtotime($comment['updated']))).'</p>
-                                    <p class="text-muted">Vytvořeno: '.htmlspecialchars(date('d.m.Y H:i',strtotime($comment['created']))).'</p>
-                                    <a href="comment.php='.$comment['comments_id'].'" class="btn btn-primary">Upravit</a>
-                                    <a href="deleteComment.php?id='.$comment['comments_id'].'" class="btn btn-danger">Smazat</a>
-                                </div>
+                                    <p class="text-muted">Vytvořeno: '.htmlspecialchars(date('d.m.Y H:i',strtotime($comment['created']))).'</p>';
+
+                                    if ($comment['users_id'] == $_SESSION['users_id'] || $_SESSION['isAdmin'] ){
+                                        echo '<a href="comment.php='.$comment['comments_id'].'" class="btn btn-primary">Upravit</a>
+                                    <a href="deleteComment.php?id='.$comment['comments_id'].'" class="btn btn-danger">Smazat</a>';
+                                    }
+                            echo '</div>
                             </div>
                       </div>';
         }
     }
     else{
         echo '<div class="container p-5 my-5 border border-3">
-                    <h2>Nebyly nalezeny žádné kategorie.</h2>
+                    <h2>Nebyly nalezeny žádné příspěvky.</h2>
                   </div>';
     }
 
--- userLikes.php
+++ userLikes.php
@@ -4,13 +4,15 @@
 
     include 'include/header.php';
 
-    $comments = $db->prepare('SELECT creator.username AS creator, comments.comments_id, comments.text AS text, topics.name AS topic_name, categories.name AS category_name, users.username AS username, comments.created AS created, comments.updated AS updated
+    $query = $db->prepare('SELECT categories.categories_id AS categories_id, topics.topics_id AS topics_id, creator.users_id AS users_id, creator.username AS creator, comments.comments_id, comments.text AS text, topics.name AS topic_name, categories.name AS category_name, users.username AS username, comments.created AS created, comments.updated AS updated
                                     FROM users JOIN likes ON users.users_id=likes.users_id JOIN comments ON likes.comments_id=comments.comments_id JOIN users AS creator ON creator.users_id=comments.creator_id JOIN topics ON topics.topics_id=comments.topics_id JOIN categories ON categories.categories_id=topics.categories_id
                                     WHERE users.users_id=:users_id
                                     ORDER BY updated DESC;');
-    $comments->execute([
+    $query->execute([
         ':users_id'=> $_SESSION['users_id']
     ]);
+
+    $comments = $query->fetchAll(PDO::FETCH_ASSOC);
 
     if (!empty($comments)){
         foreach ($comments as $comment) {
@@ -19,22 +21,25 @@
                                 <div class="col-8">
                                     <h2>'.htmlspecialchars($comment['creator']).'</h2>
                                     <p>'.htmlspecialchars($comment['text']).'</p>
-                                    <span class="badge bg-primary">'.$comment['category_name'].'</span>
-                                    <span class="badge bg-secondary">'.$comment['topic_name'].'</span>
+                                    <a href="showTopics.php?id='.$comment['categories_id'].'" class="badge bg-primary">'.$comment['category_name'].'</a>
+                                    <a href="showComments.php?id='.$comment['topics_id'].'" class="badge bg-secondary">'.$comment['topic_name'].'</a>
                                 </div>
                                 <div class="col-4">
                                     <p class="text-muted">Aktualizováno: '.htmlspecialchars(date('d.m.Y H:i',strtotime($comment['updated']))).'</p>
-                                    <p class="text-muted">Vytvořeno: '.htmlspecialchars(date('d.m.Y H:i',strtotime($comment['created']))).'</p>
-                                    <a href="comment.php='.$comment['comments_id'].'" class="btn btn-primary">Upravit</a>
-                                    <a href="deleteComment.php?id='.$comment['comments_id'].'" class="btn btn-danger">Smazat</a>
-                                </div>
+                                    <p class="text-muted">Vytvořeno: '.htmlspecialchars(date('d.m.Y H:i',strtotime($comment['created']))).'</p>';
+
+                                    if ($comment['users_id'] == $_SESSION['users_id'] || $_SESSION['isAdmin'] ){
+                                        echo '<a href="comment.php='.$comment['comments_id'].'" class="btn btn-primary">Upravit</a>
+                                    <a href="deleteComment.php?id='.$comment['comments_id'].'" class="btn btn-danger">Smazat</a>';
+                                    }
+                            echo '</div>
                             </div>
                       </div>';
         }
     }
     else{
         echo '<div class="container p-5 my-5 border border-3">
-                    <h2>Nebyly nalezeny žádné kategorie.</h2>
+                    <h2>Nebyly nalezeny žádné příspěvky.</h2>
                   </div>';
     }
 
