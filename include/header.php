443306d75a5ca988587c67fb55ad3992d367184b
Petr Sychra <sychrapetan@seznam.cz>
2023-05-24 18:23:09 +0200

    přidání knihoven

--- include/header.php
+++ include/header.php
@@ -8,6 +8,10 @@
     <title><?php echo (!empty($title)?$title.' - ':'')?>Fórum</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <script src="include/like_dislike.js"></script>
+    <link rel="stylesheet" type="text/css" href="include/style.css">
 </head>
 <body>
     <header class="p-5 bg-primary text-white text-center">
