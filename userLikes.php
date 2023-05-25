2a76ed38cd83d91d09a82c7923dd22344a3d19fd
sycp00 <sycp00@vse.cz>
2023-05-25 23:35:47 +0200

    bugfix

--- userLikes.php
+++ userLikes.php
@@ -5,11 +5,6 @@
     include 'include/header.php';
     include 'include/functions.php';
 
-    if(!isset($_SESSION['users_id'])){
-        header('Location: index.php');
-    }
-
-
     ?>
 
     <div class="row">
